252a9fe2eaa810ce6d7952685a27a2340966a083
AlexNbl27 <alexandre.noblet@ynov.com>
2024-01-19 16:09:52 +0100

    Ajoutez des fichiers projet.

---
+++ GymGenius/Librairy/SplashWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace GymGenius
+{
+    /// <summary>
+    /// Logique d'interaction pour SplashWindow.xaml
+    /// </summary>
+    public partial class SplashWindow : Window
+    {
+        public SplashWindow()
+        {
+            InitializeComponent();
+            Show();
+        }
+    }
+}
