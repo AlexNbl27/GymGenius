f8f71be2e515f1ee5bcab238315d3a0560a58664
Manon LE BARAZER <manonlebarazer@icloud.com>
2024-01-23 18:23:34 +0100

    front page home sympa

--- GymGenius/Views/Home.xaml.cs
+++ GymGenius/Views/Home.xaml.cs
@@ -16,9 +16,55 @@ namespace GymGenius
     /// </summary>
     public partial class MainWindow : Window
     {
+   
         public MainWindow()
         {
             InitializeComponent();
+            InitializePlaceholder();
+        }
+
+        private void InitializePlaceholder()
+        {
+            ShowPlaceholder();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            HidePlaceholder();
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(coucou.Text))
+            {
+                ShowPlaceholder();
+            }
+        }
+
+        private void ShowPlaceholder()
+        {
+            coucou.Text = "Rechercher un exercice en particulier"; 
+            coucou.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#25322C"));
+            coucou.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#33443C"));
+        }
+
+        private void HidePlaceholder()
+        {
+            if (coucou.Text == "Rechercher un exercice en particulier")
+            {
+                coucou.Text = string.Empty;
+                coucou.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#25322C"));
+                coucou.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#33443C"));
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Gérer le clic du bouton
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            // La case à cocher a été cochée, exécutez votre logique ici
         }
     }
 }
