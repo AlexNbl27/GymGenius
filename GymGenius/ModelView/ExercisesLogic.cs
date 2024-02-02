451b640f05fca4019161b844544ad80fb84b37a6
Alexandre NOBLET <alexandre.noblet@ynov.com>
2024-02-02 11:05:30 +0100

    Updates and clean code

---
+++ GymGenius/ModelView/ExercisesLogic.cs
@@ -0,0 +1,224 @@
+﻿using GymGenius.Models;
+
+namespace GymGenius.ModelView
+{
+    public class ExercisesLogic
+    {
+
+        private List<AExercise> allExercises = new List<AExercise>
+        {
+            new PushUps(),
+            new PullUps(),
+            new Dips(),
+            new BenchPress(),
+            new InclinedBenchPress(),
+            new ForeheadBar(),
+            new PulleyTricepsExtensions(),
+            new CableMiddleFly(),
+            new Crunch(),
+            new SideElevation(),
+            new FrontalElevation(),
+            new BirdElevation(),
+            new MilitaryPress(),
+            new InvertedPecDeck(),
+            new FacePull(),
+            new HorizontalDraft(),
+            new Deadlift(),
+            new BicepsCurl(),
+            new CurlRotation(),
+            new PulleyCurl(),
+            new Squat(),
+            new LegPress(),
+            new Slots(),
+            new LegsExtensions(),
+            new LegsExtensions(),
+            new CalfPress(),
+            new Treadmill(),
+            new Rowing(),
+            new Stairs(),
+            new Elliptical()
+        };
+        public List<AExercise> GetAllExercises()
+        {
+            return this.allExercises;
+        }
+
+        public List<AExercise> GetFilteredExercises(bool legs, bool arms, bool back, bool shoulders, bool trunk, bool cardio, bool muscular, int difficulty = 99)
+        {
+            List<AExercise> filteredExercises = new List<AExercise>();
+            if (legs)
+            {
+                filteredExercises.AddRange(this.GetLegsExercises());
+            }
+            if (arms)
+            {
+                filteredExercises.AddRange(this.GetArmsExercises());
+            }
+            if (back)
+            {
+                filteredExercises.AddRange(this.GetBackExercises());
+            }
+            if (shoulders)
+            {
+                filteredExercises.AddRange(this.GetShouldersExercises());
+            }
+            if (trunk)
+            {
+                filteredExercises.AddRange(this.GetTrunkExercises());
+            }
+
+            // Remove duplicates
+            filteredExercises = filteredExercises.Distinct().ToList();
+
+
+            if (cardio)
+            {
+                filteredExercises = FilterCardioExercises(filteredExercises);
+            }
+            if (muscular)
+            {
+                filteredExercises = FilterMuscularExercises(filteredExercises);
+            }
+            if (difficulty != 99)
+            {
+                filteredExercises = FilterExercisesByDifficulty(filteredExercises, difficulty);
+            }
+
+            return filteredExercises;
+        }
+
+        private List<AExercise> GetLegsExercises()
+        {
+            List<AExercise> legsExercises = new List<AExercise>();
+            foreach (AExercise exercise in this.allExercises)
+            {
+                foreach (AMuscles muscle in exercise.Tags.Muscles)
+                {
+                    if (muscle.bodyPart is Legs)
+                    {
+                        legsExercises.Add(exercise);
+                        break;
+                    }
+                }
+            }
+            return legsExercises;
+        }
+
+        private List<AExercise> GetArmsExercises()
+        {
+            List<AExercise> armsExercises = new List<AExercise>();
+            foreach (AExercise exercise in this.allExercises)
+            {
+                foreach (AMuscles muscle in exercise.Tags.Muscles)
+                {
+                    if (muscle.bodyPart is Arms)
+                    {
+                        armsExercises.Add(exercise);
+                        break;
+                    }
+                }
+            }
+            return armsExercises;
+        }
+
+        private List<AExercise> GetBackExercises()
+        {
+            List<AExercise> chestExercises = new List<AExercise>();
+            foreach (AExercise exercise in this.allExercises)
+            {
+                foreach (AMuscles muscle in exercise.Tags.Muscles)
+                {
+                    if (muscle.bodyPart is Back)
+                    {
+                        chestExercises.Add(exercise);
+                        break;
+                    }
+                }
+            }
+            return chestExercises;
+        }
+
+        private List<AExercise> GetShouldersExercises()
+        {
+            List<AExercise> shouldersExercises = new List<AExercise>();
+            foreach (AExercise exercise in this.allExercises)
+            {
+                foreach (AMuscles muscle in exercise.Tags.Muscles)
+                {
+                    if (muscle.bodyPart is Shoulders)
+                    {
+                        shouldersExercises.Add(exercise);
+                        break;
+                    }
+                }
+            }
+            return shouldersExercises;
+        }
+
+        private List<AExercise> GetTrunkExercises()
+        {
+            List<AExercise> trunkExercises = new List<AExercise>();
+            foreach (AExercise exercise in this.allExercises)
+            {
+                foreach (AMuscles muscle in exercise.Tags.Muscles)
+                {
+                    if (muscle.bodyPart is Trunk)
+                    {
+                        trunkExercises.Add(exercise);
+                        break;
+                    }
+                }
+            }
+            return trunkExercises;
+        }
+
+        private List<AExercise> FilterCardioExercises(List<AExercise> filteredExercises)
+        {
+            List<AExercise> cardioExercises = new List<AExercise>();
+
+            foreach (AExercise exercise in filteredExercises)
+            {
+                if (exercise.Type is Cardio)
+                {
+                    cardioExercises.Add(exercise);
+                }
+            }
+
+            return cardioExercises;
+        }
+
+
+        private List<AExercise> FilterMuscularExercises(List<AExercise> filteredExercises)
+        {
+            List<AExercise> muscularExercises = new List<AExercise>();
+
+            foreach (AExercise exercise in filteredExercises)
+            {
+                if (exercise.Type is Muscular)
+                {
+                    muscularExercises.Add(exercise);
+                }
+            }
+
+            return muscularExercises;
+        }
+
+        private List<AExercise> FilterExercisesByDifficulty(List<AExercise> filteredExercises, int difficulty)
+        {
+            List<AExercise> filteredByDifficultyExercises = new List<AExercise>();
+
+            foreach (AExercise exercise in filteredExercises)
+            {
+                if (exercise.Level <= difficulty)
+                {
+                    filteredByDifficultyExercises.Add(exercise);
+                }
+            }
+
+            return filteredByDifficultyExercises;
+        }
+
+
+
+    }
+}
---
+++ GymGenius/Views/ExercisesWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using GymGenius.Models;
+using GymGenius.ModelView;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace GymGenius.Views
+{
+    public partial class ExercisesWindow : Window
+    {
+        // Property to store the list of exercises
+        public List<AExercise> Exercises { get; set; }
+        public List<AExercise> selectedExercises { get; set; } = new List<AExercise>();
+        public Session Session { get; set; }
+
+        // ===== CONSTRUCT FUNCTIONS ===== //
+
+        public ExercisesWindow(Session session)
+        {
+            InitializeComponent();
+            ExercisesLogic exercisesLogic = new();
+            Exercises = exercisesLogic.GetAllExercises();
+            this.Session = session;
+
+            // Set the DataContext to this window so that bindings work
+            DataContext = this;
+        }
+
+        public ExercisesWindow(Session session, List<AExercise> filteredExercises)
+        {
+            InitializeComponent();
+            Exercises = filteredExercises;
+            this.Session = session;
+
+            // Set the DataContext to this window so that bindings work
+            DataContext = this;
+        }
+
+
+        // ===== CLICK FUNCTIONS ===== //
+
+        private void CreateSessionButton(object sender, RoutedEventArgs e)
+        {
+            if (this.selectedExercises.Count != 0)
+            {
+                this.Session.exercises = this.selectedExercises;
+                RecapSessionWindow seance = new(this.Session);
+                seance.Show();
+            }
+            else
+            {
+                MessageBox.Show("Sélectionnez au moins un exercice !");
+            }
+        }
+
+        private void OnSearch_click(object sender, RoutedEventArgs e)
+        {
+            this.Exercises.Clear();
+            List<AExercise> tmp = new List<AExercise>();
+            ExercisesLogic exercisesLogic = new();
+
+            if (dos.IsChecked.GetValueOrDefault() || jambe.IsChecked.GetValueOrDefault() || bras.IsChecked.GetValueOrDefault() || epaule.IsChecked.GetValueOrDefault() || tronc.IsChecked.GetValueOrDefault() || numericTextBox.Text != "" || cardio.IsChecked.GetValueOrDefault() || musculaire.IsChecked.GetValueOrDefault())
+            {
+                if (numericTextBox.Text != "")
+                {
+                    tmp = exercisesLogic.GetFilteredExercises(jambe.IsChecked.GetValueOrDefault(), bras.IsChecked.GetValueOrDefault(), dos.IsChecked.GetValueOrDefault(), epaule.IsChecked.GetValueOrDefault(), tronc.IsChecked.GetValueOrDefault(), cardio.IsChecked.GetValueOrDefault(), musculaire.IsChecked.GetValueOrDefault(), int.Parse(numericTextBox.Text));
+                }
+                else
+                {
+                    tmp = exercisesLogic.GetFilteredExercises(jambe.IsChecked.GetValueOrDefault(), bras.IsChecked.GetValueOrDefault(), dos.IsChecked.GetValueOrDefault(), epaule.IsChecked.GetValueOrDefault(), tronc.IsChecked.GetValueOrDefault(), cardio.IsChecked.GetValueOrDefault(), musculaire.IsChecked.GetValueOrDefault());
+                }
+            }
+            else
+            {
+                tmp = exercisesLogic.GetAllExercises();
+            }
+
+            Close();
+            ExercisesWindow exerciceWindow = new(this.Session, tmp);
+            exerciceWindow.Show();
+        }
+
+        // ===== INPUT FUNCTIONS ===== //
+
+        private void NumericTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+            {
+                e.Handled = true;
+            }
+
+            if (int.TryParse(numericTextBox.Text + e.Text, out int result) && result > 5)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void numericTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox clickedCheckBox = sender as CheckBox;
+
+            // If the first checkbox is checked, uncheck the second checkbox
+            if (clickedCheckBox == cardio && cardio.IsChecked == true)
+            {
+                musculaire.IsChecked = false;
+            }
+            // If the second checkbox is checked, uncheck the first checkbox
+            else if (clickedCheckBox == musculaire && musculaire.IsChecked == true)
+            {
+                cardio.IsChecked = false;
+            }
+        }
+
+        // ===== ACTIONS FUNCTIONS ===== //
+
+        public event EventHandler<AExercise> ExerciseClicked;
+
+        private void ExerciseComponent_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Components.ExerciseComponent exerciseComponent)
+            {
+                // Change border color to blue when mouse button is pressed
+                if (exerciseComponent.FindName("ExerciseBorder") is Border border)
+                {
+                    exerciseComponent.BorderBrush = System.Windows.Media.Brushes.Blue;
+                }
+            }
+        }
+
+        private void ExerciseComponent_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Components.ExerciseComponent exerciseComponent)
+            {
+                // Change border color back to black when mouse button is released
+                if (exerciseComponent.FindName("ExerciseBorder") is Border border)
+                {
+                    exerciseComponent.BorderBrush = System.Windows.Media.Brushes.Black;
+                }
+
+                if (exerciseComponent.DataContext is AExercise clickedExercise)
+                {
+                    // Add the clicked exercise to selectedExercises
+                    selectedExercises.Add(clickedExercise);
+                }
+            }
+        }
+
+    }
+}
